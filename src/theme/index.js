dde75e62471400051c7ec9e5bc98afcb39517bb6
Matthew Stanciu <mattbstanciu@gmail.com>
2019-06-30 15:13:10 -0400

    reorganize theme files

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,24 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
-
-const colors = {
-    cream: '#EFE3D5',
-    red: '#DB3847',
-    blue: '#406FC9',
-    lightBlue: '#00A0D4',
-    turquoise: '#1FA49F'
-}
-
-const font = 'IBM Plex Sans'
-
-const space = [0, 4, 8, 16, 32, 64, 128, 256, 512]
-const fontSizes = [12, 14, 16, 18, 24, 32, 48, 64, 72, 96]
-
-export const theme = {
-    colors,
-    font,
-    space,
-    fontSizes
-}
+import React from 'react'
+import { createGlobalStyle, ThemeProvider as Root } from 'styled-components'
+import theme from './theme'
 
 export const GlobalStyle = createGlobalStyle`
   * {
---
+++ src/theme/theme.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+const colors = {
+    cream: '#EFE3D5',
+    red: '#DB3847',
+    blue: '#406FC9',
+    lightBlue: '#00A0D4',
+    turquoise: '#1FA49F'
+}
+
+const font = 'IBM Plex Sans'
+
+const space = [0, 4, 8, 16, 32, 64, 128, 256, 512]
+const fontSizes = [12, 14, 16, 18, 24, 32, 48, 64, 72, 96]
+
+export const theme = {
+    colors,
+    font,
+    space,
+    fontSizes
+}
