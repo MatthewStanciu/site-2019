c6fe3ab5f5c0109e8dcf132109d37b8878257d9b
Matthew Stanciu <mattbstanciu@gmail.com>
2019-07-25 04:44:19 -0400

    Add social cards

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import ThemeProvider from '../theme'
+import theme from '../theme/config'
+import { name, title, description, url, img } from '../data.json'
+
+const meta = tags =>
+  tags.map((props, index) =>
+    React.createElement('meta', { ...props, key: index })
+  )
 
 export default ({ children }) => (
   <ThemeProvider>
@@ -15,6 +22,23 @@ export default ({ children }) => (
         href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700&display=swap"
         rel="stylesheet"
       />
+      <meta name="theme-color" content={theme.colors.red} />
+      {meta([
+        { name: 'description', content: description },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:description', content: description },
+        { name: 'twitter:domain', content: url },
+        { name: 'twitter:site', content: url },
+        { name: 'twitter:image', content: img },
+        { name: 'twitter:title', content: title },
+        { property: 'og:description', content: description },
+        { property: 'og:image', content: img },
+        { property: 'og:locale', content: 'en_US' },
+        { property: 'og:site_name', content: name },
+        { property: 'og:title', content: title },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: url }
+      ])}
     </Helmet>
     {children}
   </ThemeProvider>
